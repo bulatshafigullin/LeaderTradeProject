7c42ca9355df7a9ccf5d0194bc073c4669910eb2
bulatsh <bulatsh]>
2024-01-15 19:43:26 +0300

    fix create interval

--- server/src/apps/unloads/management/commands/createintervals.py
+++ server/src/apps/unloads/management/commands/createintervals.py
@@ -5,11 +5,11 @@ from django_celery_beat.models import IntervalSchedule
 class Command(BaseCommand):
     def handle(self, *args, **options) -> None:
         # The magic line
-        IntervalSchedule.objects.create(every=1, period="minutes")
-        IntervalSchedule.objects.create(every=5, period="minutes")
-        IntervalSchedule.objects.create(every=30, period="minutes")
-        IntervalSchedule.objects.create(every=1, period="hours")
-        IntervalSchedule.objects.create(every=5, period="hours")
-        IntervalSchedule.objects.create(every=12, period="hours")
-        IntervalSchedule.objects.create(every=1, period="days")
+        IntervalSchedule.objects.get_or_create(every=1, period="minutes")
+        IntervalSchedule.objects.get_or_create(every=5, period="minutes")
+        IntervalSchedule.objects.get_or_create(every=30, period="minutes")
+        IntervalSchedule.objects.get_or_create(every=1, period="hours")
+        IntervalSchedule.objects.get_or_create(every=5, period="hours")
+        IntervalSchedule.objects.get_or_create(every=12, period="hours")
+        IntervalSchedule.objects.get_or_create(every=1, period="days")
         print("Time intervals created")
